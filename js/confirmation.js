ee853cb9fa500ac001033f5b4456ebf9b74a5a5c
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-03-30 16:14:03 +0200

    Add files via upload

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,42 +1,28 @@
-
 /********************CONFIRMATION PAGE********************************/
 //variables
 let sum = document.getElementById("result");
 let resume = document.getElementById("resume");
-
 let productsInLocal =JSON.parse(localStorage.getItem("data"));
-console.log('Products localStorage', productsInLocal);
 let respInfoServer =JSON.parse(localStorage.getItem("infoProduit"));
-console.log('info back from server',respInfoServer);
-
 let somme=0 ; 
 
 /*************STRUCTURE PAGE **********************/
-
 //Thank you message 
-
-  console.log('orderID', JSON.parse(localStorage.getItem("orderId")))
-  
-	resume.innerHTML =`
-     
-    <div class="MerciMsg">
+resume.innerHTML =`
+<div class="MerciMsg">
  	<p>MERCI POUR VOTRE COMMANDE</p>
  	<p>Votre numéro de commande est ${JSON.parse(localStorage.getItem("orderId"))}</p>
-    </div>
-
-     `
+</div> `
 
 // Total basket
 productsInLocal.forEach((items,index)=>{
-
 	somme +=items.price;
 	sum.innerHTML=`
       <p>Le total de vos achats est de ${somme +" "+"€"}</p>
-	
 	`
 });
 
-/***********************************************/
+
 
 
 
