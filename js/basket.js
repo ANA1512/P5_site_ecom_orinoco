e947bb1871bf2387d26a44a00d78e1bff556ddf6
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-04-01 12:20:00 +0200

    Add files via upload

--- js/basket.js
+++ js/basket.js
@@ -1,5 +1,6 @@
-/******************BASKET PAGE*********************/
 
+/******************BASKET PAGE*********************/
+//Variables for the BASKET
 //All products selected and store
 let localBasket=  JSON.parse(localStorage.getItem('data'));
 
@@ -21,9 +22,9 @@ let itemsCart =document.getElementById("itemsCart");
 localBasket.forEach(element => {
  addQuantity(element);
 });
-
 // ADD quantity (in editedBasket)
-function addQuantity(obj) { 
+function addQuantity(obj) {
+    
     if (obj._id in index ) {
        index[obj._id].quantity += 1;
    } else {
@@ -32,52 +33,61 @@ function addQuantity(obj) {
        editedBasket.push(obj);
    }
 };
-
-/****************DELETE ONE PRODUCT (icone trash)*************/
- function suppression(id){       
-    let list= localBasket.filter(item => {
+/****************DELETE ONE PRODUCT (icone) ******************************/
+function suppression(id){
+       let list= localBasket.filter(item => {
       return item._id != id;
     });
-    localBasket=list;
-    localStorage.setItem('data', JSON.stringify(list));
-    location.reload();
+     localBasket=list;
+     console.log(localBasket);
+     console.log(editedBasket);
+     localStorage.setItem('data', JSON.stringify(list));
+     location.reload();
  };
 
-/***********************PAGE STRUCTURE*************************/
+/***********************PAGE STRUCTURE***********************************/
 function display(){ 
-    let itemsDisplay= document.getElementById("items");
-    //Table structure 
-    itemsDisplay.innerHTML ='' 
-    //Loop forEach for Item in new Basket
-    editedBasket.forEach((items,index)=>{ 
-    //Display all products in HTML page
-    itemsDisplay.innerHTML += `  
-      <div class= "prod">
-      <div class= "pict">
-        <img src=${editedBasket[index].imageUrl} class="cartpict">${editedBasket[index].lenses[index]}</td>
-      </div>
-      <div class="infoPict">
-        <p>${editedBasket[index].name}</p>
-        <input type="number" value='${editedBasket[index].quantity}' id="quantite" >
-        <p>${editedBasket[index].price}€</p>
-      <div id="supprimer" onclick="suppression('${editedBasket[index]._id}')"><i class="far fa-trash-alt"></i></div>
-      </div>
-      </div>
-         `
-    //Total (sum of all products)
-    totalAllProd.innerHTML ="Total:" +" "+(total+=editedBasket[index].price*editedBasket[index].quantity)+ "€"+" "+"("+localBasket.length+")";     
-  });
-};
-display();
+   let itemsDisplay= document.getElementById("items");
 
-//***********EMPTY BASKET *********************/
-const spanpv = document.querySelector("#pv");  
+        //Table structure 
+        itemsDisplay.innerHTML =''
+        
+        //Loop forEach for Item in new Basket
+        editedBasket.forEach((items,index)=>{ 
+        
+        //Display all products in HTML page
+         itemsDisplay.innerHTML += `  
+         
+         <div class= "prod">
+         <div class= "pict">
+         <img src=${editedBasket[index].imageUrl} class="cartpict">${editedBasket[index].lenses[index]}</td>
+         </div>
+
+         <div class="infoPict">
+         <p>${editedBasket[index].name}</p>
+         <input  type="number" value='${editedBasket[index].quantity}' id="quantite" >
+
+         <p>${editedBasket[index].price}€</p>
+         <div id="supprimer" onclick="suppression('${editedBasket[index]._id}')"><i class="far fa-trash-alt"></i></div>
+         </div>
+         </div>
+         
+         `
+        //Total (sum of all products)
+        totalAllProd.innerHTML ="Total:" +" "+(total+=editedBasket[index].price*editedBasket[index].quantity)+ "€"+" "+"("+localBasket.length+")";     
+       });
+    };
+
+    display();
+
+//****EMPTY BASKET ( editedbasket et localStorage)*******/
+const spanpv = document.querySelector("#pv");   
  if(localBasket.length>= 1){
   spanpv.remove();
   };
 
-/*************************FORMS********************/
-
+/*************************FORMS********************************************************/
+//variables 
 let inputs = document.getElementsByTagName("input");
 let mail = document.getElementById("mail");
 let myName = document.getElementById("nom");
@@ -89,13 +99,14 @@ let buttonForm = document.getElementById("button");
 let inscription = document.getElementById("inscription");
 let msgMail= document.getElementById("msgMail");
 let msgConfir= document.getElementById("msgConfir");
+let errorFormat = document.getElementById("erreur");
 
-/********* OBJET  REGEXP *******/
+/********* REGEXP *******/
 // validation mail
 const emailRegExp ='^[ a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$';
-// validation nom-prenom-ville 
+// nom prenom ville 
 const civilId ='^[a-zA-Z]{3,30}$';
-//rue 
+// rue 
 const streetBuyer = '^[a-zA-Z0-9]';
 
 /****STRUCTURE FORM********/
@@ -106,130 +117,147 @@ contact.appendChild(infoPerso);
 
 /****************Test de Vérification du champs mail uniquement*******/
 function checkMail(){
-// Test email valid
-    if(mail.value.match(emailRegExp)){
-        msgMail.textContent="Votre email est valide"; 
-        msgMail.style.color="lightgreen";
-        buttonForm.style.visibility="visible";
-// Test email field empty        
-    }else if(mail.value ==''){
-        msgMail.textContent="Veuillez remplir le champs";
-        msgMail.style.color="blue";
-        buttonForm.style.visibility="hidden";
-    }
-//Test mail  incorrect regexp
-    else if(!mail.value.match(emailRegExp)){ 
-        msgMail.textContent= "Votre email est incorrecte";
-        msgMail.style.color="red";
-        buttonForm.style.visibility="hidden"; 
-    }
+        // Test email valid
+         if(mail.value.match(emailRegExp)){
+          msgMail.textContent="Votre email est valide"; // mail valid
+          msgMail.style.color="lightgreen";
+          buttonForm.style.visibility="visible";
+
+        // Test email field empty        
+        }else if(mail.value ==''){
+         msgMail.textContent="Veuillez remplir le champs";
+         msgMail.style.color="blue"; // couleur du message 
+         buttonForm.style.visibility="hidden";    
+       }
+        //Test mail  incorrect regexp
+        else if(!mail.value.match(emailRegExp)){ 
+          msgMail.textContent= "Votre email est incorrecte";
+          msgMail.style.color="red";
+          buttonForm.style.visibility="hidden"; 
+        }
 };
-/************** validity INPUT (NAME- FIRSTNAME-CITY)***********/
+/************** INPUT CITY **************************** */
 function checkCivil() {
-    if(myName.value.match(civilId) 
-    && myFirstName.value.match(civilId) 
-    && myTown.value.match(civilId)){
-    console.log("correct**********$")
-    buttonForm.style.visibility="visible"; 
+    if(myTown.value.match(civilId)){
+      console.log("****** format ville valide ******");
+      buttonForm.style.visibility="visible"; 
     }else{
-    console.log("---------NOP-------")
-    //alert("Vérifier le format de vos champs!");
-    console.log(myName.value);
-    buttonForm.style.visibility="hidden";    
+      buttonForm.style.visibility="hidden";
   }
 };
-//***************TEST STREET ************************/
-function checkStreet(){
-    if(myStreet.value.match(streetBuyer)) {
-    console.log("correct   STREET **********$")
+//-----------INPUT NAME --------------------------
+function checkName(){
+   if(myName.value.match(civilId)){
+     console.log("****** format nom valide ******");
+     buttonForm.style.visibility="visible";
+   }else{
+    buttonForm.style.visibility="hidden";
+    return
+   }
+};
+//-------------INPUT PRENOM------------------------
+function checkFirstName(){
+  if(myFirstName.value.match(civilId)){
+    buttonForm.style.visibility="visible";
   }else{
-   console.log("---------NOP  STREET -------")
-    //alert("Vérifier le format de vos champs!");
+   buttonForm.style.visibility="hidden";
+   return
   }
-
 };
-
-// /******************fields validation ****************/
+//-----------INPUT STREET --------------------
+function checkStreet(){
+  if(myStreet.value.match(streetBuyer)) { 
+    console.log("****** format rue valide ******");
+    buttonForm.style.visibility="visible";
+  }else{
+    buttonForm.style.visibility="hidden";
+    return
+  }
+};
+//------------Validation de tous les inputs----------
 //Validation for all fields
 let formVal = document.getElementById("inscription");
-  formVal.addEventListener("submit", function(e){
-  e.preventDefault();
-// Get inputs and save it in Constructor
-let inputName= document.getElementById("nom").value;
-let inputFirstName = document.getElementById("prenom").value;
-let inputRue = document.getElementById("rue").value;
-let inputVille= document.getElementById("ville").value; 
-let inputMail = document.getElementById("mail").value; 
+   formVal.addEventListener("submit", function(e){
+    e.preventDefault();
+ // Get inputs and save it in Constructor
+ let inputName= document.getElementById("nom").value;
+ let inputFirstName = document.getElementById("prenom").value;
+ let inputRue = document.getElementById("rue").value;
+ let inputVille= document.getElementById("ville").value; 
+ let inputMail = document.getElementById("mail").value; 
+
+
+// verif champ correct regex pas d'envoi 
+if (inputName.match(civilId)
+  && inputFirstName.match(civilId)
+  &&inputRue.match(streetBuyer)
+  && inputVille.match(civilId)
+  && inputMail.match(emailRegExp)){
+        console.log( " les champs sont valides");
+    }else{
+        console.log ( "problème de champs ")
+      return
+   }
+
 // verif que les input sont remplis
-if ( inputName.length<1 
+  if ( inputName.length<1 
     || inputFirstName.length<1 
     || inputRue.length<1 
     || inputVille.length<1 
     || inputMail.length<1){
-    console.log("champs invalide");
-      alert("Veuillez remplir les champs du formulaire");
-      return
-}else{
-      console.log("champs valide ");
-} 
-/*Save data in localStorage*/
-//get product id from the basket
-let products=[];
-const productSelected=[];
 
-for(i=0; i<editedBasket.length; i++){
-  productSelected.push(editedBasket[i]._id);
-}
-//Constructor pour récupérer les infos contact et produits du formulaire
-  order={
-    contact: {
-      firstName:inputFirstName,
-      lastName: inputName,
-      address:inputRue ,
-      city: inputVille,
-      email: inputMail
-    },
-    products: productSelected
-}
-localStorage.setItem("infoProduit", JSON.stringify(order));
+    alert("Veuillez remplir les champs du formulaire");
+    return
+    
+    }else{
+    console.log(" Tous  les champs sont remplis ");
+    }
+
       
+ /*Save data in localStorage*/
+//get product id from the basket
+      let products=[];
+      const productSelected=[];
+
+      for(i=0; i<editedBasket.length; i++){   
+      productSelected.push(editedBasket[i]._id);
+      }
+// Constructor pour récupérer les infos contact et produits du formulaire
+      order={
+          contact: {
+              firstName:inputFirstName,
+              lastName: inputName,
+              address:inputRue ,
+              city: inputVille,
+              email: inputMail
+          },
+            products: productSelected
+        }
+      localStorage.setItem("infoProduit", JSON.stringify(order));
 /****************ENVOI DU FORMULAIRE A L API*************************/
-fetch('http://localhost:3000/api/cameras/order',{
-    method: "POST",
-    body:JSON.stringify(order),
-    headers: {"Content-Type":"application/json"}
-})
-    .then(response=> response.json())
-//save info and orderId create in localStorage
-    .then((data) =>{
-    localStorage.setItem("orderId", JSON.stringify(data.orderId)) ; 
-    document.location.href ="confirmation_page.html";
-})
-// if error request invalid
-    if(erreur){
-      console.log('Requête non autorisé:',"Votre panier est INVALIDE");
-      //if no error request valid
-       }else{
-         console.log( 'Requête autorisé:',"Votre panier est VALIDE")
-       }
-});
+       fetch('http://localhost:3000/api/cameras/order',{
+            method: "POST",
+            body:JSON.stringify(order),
+            headers: {"Content-Type":"application/json"}
+       })
+       .then(response=> response.json())
+        //save info and orderId create in localStorage
+          .then((data) =>{
+          localStorage.setItem("orderId", JSON.stringify(data.orderId)) ; 
+          document.location.href ="confirmation_page.html";
+         })
+        
+        // if error request invalid
+         if(erreur){
+          console.log("requête invalide");
+        //if no error request valid
+          }else{
+         console.log( "requête valide") 
+       } 
+    });
   
 
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-   
 
     
 
